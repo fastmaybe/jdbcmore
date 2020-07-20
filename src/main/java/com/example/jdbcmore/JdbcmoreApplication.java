5869f6748dc006aea06905a880798b951aee746e
yuan <18672793611@163.com>
2020-07-20 21:58:53 +0800

    init

---
+++ src/main/java/com/example/jdbcmore/JdbcmoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcmoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdbcmore/JdbcmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcmoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
